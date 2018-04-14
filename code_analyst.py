c55f046c9f6aa0614ea97b0677ba64431aa73f8f
Lee Chun On <jeffreyleeon@gmail.com>
2018-04-13 21:41:08 -0700

    Parse command line params

--- code_analyst.py
+++ code_analyst.py
@@ -9,20 +9,50 @@ sys.path.insert(0, os.path.join(base_dir, "./lib"))
 from common.type_validator import ArrayValidator as array_validator
 import common.constants as constants
 
-def main(opt_type, opt_params):
+config = {
+    'operation_type': None,
+    'github_url': None
+}
+
+def parse_config(opt_params):
+    opts, args = getopt.getopt(opt_params, 'h', ['url=', 'operation-type='])
+    for opt, arg in opts:
+        arg = str(arg)
+        if opt == '--url':
+            if constants.GITHUB_BASE_URL not in arg:
+                raise Exception('Expect url to include {}'.format(arg))
+            config['github_url'] = arg
+        elif opt == '--operation-type':
+            if arg not in constants.CODE_ANALYST_OPT_TYPES.values():
+                raise Exception('Unknown operation type: {}'.format(arg))
+            config['operation_type'] = arg
+        elif opt == '-h':
+            usage()
+            sys.exit()
+
+def usage():
+    print "python populate_throttle_devices.py --url github_https_url/--operation-type operation_type/-h"
+    print "     -h                  Print this message"
+    print "     --url               github url in https, e.g. https://github.com/jeffreyleeon/code-analyst"
+    print "     --operation-type    operation types, one of {}".format(constants.CODE_ANALYST_OPT_TYPES.values())
+
+def main(opt_params):
+    try:
+        parse_config(opt_params)
+    except Exception as err:
+        print('Failed when parsing params {}\n'.format(err))
+        usage()
+        sys.exit(2)
+    print('===what is config {}'.format(config))
     pass
 
 if __name__ == '__main__':
     arguments_array = sys.argv[1:]
     # Check number of params
     if array_validator.is_non_empty_array(arguments_array) is False:
-        print('Error: Expect at least 1 argument, exit script.')
+        print('Error: Expect at least 1 argument, exit script.\n')
+        usage()
         exit(0)
-    print('Start of code analyst with arguments {}'.format(arguments_array))
-    opt_type = arguments_array[0]
-    opt_params = arguments_array[1:]
-    # Check operation type
-    if opt_type not in constants.CODE_ANALYST_OPT_TYPES.values():
-        print('Error: Unsupported operation type: {}'.format(opt_type))
-        exit(0)
-    main(opt_type, opt_params)
+    print('Start of code analyst with arguments {}\n'.format(arguments_array))
+    opt_params = arguments_array[:]
+    main(opt_params)
--- lib/common/constants.py
+++ lib/common/constants.py
@@ -4,5 +4,8 @@ constants.py contains all static values in project.
 '''
 
 CODE_ANALYST_OPT_TYPES = {
+    'ALL': 'all',
     'COMMIT_MSG': 'commit_msg'
 }
+
+GITHUB_BASE_URL = 'https://github.com/'
